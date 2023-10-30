8ed2786d4fd23810c8281113e5011dca52879ed2
Elham Qafouri <elham.qafouri.91@gmail.com>
2023-10-30 19:55:02 +0330

    add usercontroller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,6 +22,12 @@ class DatabaseSeeder extends Seeder
             'phone' => '09981928271',
             'password' => 'password'
         ]);
-
+        $user1 = User::query()->create([
+            'name' => 'test seller',
+            'email' => 'seller@gmail.com',
+            'phone' => '09981928272',
+            'password' => 'password'
+        ]);
+        $user->assignRole(Role::query()->first());
     }
 }
