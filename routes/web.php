778bb92e6640da2dc27b9a1024ce1910896891d4
Elham Qafouri <elham.qafouri.91@gmail.com>
2023-11-04 23:56:55 +0330

    create enum Role

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/', function () {
 //Route::get('register', [AuthController::class, 'showRegister'])->name('register.show');
 //Route::post('register', [AuthController::class, 'register'])->name('register');
 
-Route::prefix('auth')->group(function () {
+Route::prefix('auth')->middleware('auth')->group(function () {
     Route::get('login', [AuthController::class, 'showLogin'])->name('login.show');
     Route::post('login', [AuthController::class, 'login'])->name('login');
     Route::get('register', [AuthController::class, 'showRegister'])->name('register.show');
