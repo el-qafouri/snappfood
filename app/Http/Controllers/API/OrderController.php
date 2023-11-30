1a305a896a699cb40134d9468567b066a77c5115
Elham Qafouri <elham.qafouri.91@gmail.com>
2023-11-30 11:35:18 +0330

    add payment status to carts table

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 //use App\Http\Controllers\Controller;
+use App\Models\Cart;
 use App\Models\Discount;
 use App\Models\Food;
 use App\Models\Order;
@@ -28,6 +29,64 @@ class OrderController
         return response()->json($orders);
     }
 
+//این ادد کار میکنه ولی میخوام سبد خرید در بیارم
+//    public function add(Request $request)
+//    {
+//        $request->validate([
+//            'food_id' => 'required',
+//            'count' => 'required|integer|min:1',
+//        ]);
+//
+//        try {
+//            $food = Food::query()->find($request->food_id);
+//
+//            if (!$food) {
+//                return response(['Message' => 'Food not found.'], 404);
+//            }
+//
+//            $order = Order::query()->where([
+//                'user_id' => auth()->user()->id,
+//                'restaurant_id' => $food->restaurant->id,
+//                'customer_status' => 'unpaid',
+//            ])->first();
+//            $discounts = $food->discounts;
+////            $discountAmount = $food->discounts->sum('discount_amount');
+//            $discountAmount = $discounts ? $discounts->sum('discountAmount') : 0;
+//            $foodPrice = $discountAmount * $request->count;
+//
+//            if ($order) {
+//                $existingFood = $order->foods->find($request->food_id);
+//
+//                if ($existingFood) {
+//                    $existingFood->pivot->count += $request->count;
+//                    $existingFood->pivot->save();
+//                } else {
+//                    $order->foods()->attach($food, ['count' => $request->count]);
+//                }
+//
+//                $order->total_price += $foodPrice;
+//                $order->save();
+//            } else {
+//                $order = Order::query()->create([
+//                    'user_id' => auth()->user()->id,
+//                    'restaurant_id' => $food->restaurant->id,
+//                    'total_price' => $foodPrice,
+//                    'total_amount' => $foodPrice,
+//                ]);
+//
+//                $order->foods()->attach($food, ['count' => $request->count]);
+//            }
+//
+//            return response(['Message' => 'Food added to cart successfully', 'Cart ID' => $order->id]);
+//        } catch (\Exception $e) {
+//            \Log::error('An unexpected error occurred: ' . $e->getMessage());
+//            return response(['Message' => 'An unexpected error occurred. Please try again later.'], 500);
+//        }
+//    }
+
+
+
+
 
     public function add(Request $request)
     {
@@ -43,15 +102,15 @@ class OrderController
                 return response(['Message' => 'Food not found.'], 404);
             }
 
+            $discounts = $food->discounts;
+            $discountAmount = $discounts ? $discounts->sum('discountAmount') : 0;
+            $foodPrice = $discountAmount * $request->count;
+
             $order = Order::query()->where([
                 'user_id' => auth()->user()->id,
                 'restaurant_id' => $food->restaurant->id,
                 'customer_status' => 'unpaid',
             ])->first();
-            $discounts = $food->discounts;
-//            $discountAmount = $food->discounts->sum('discount_amount');
-            $discountAmount = $discounts ? $discounts->sum('discountAmount') : 0;
-            $foodPrice = $discountAmount * $request->count;
 
             if ($order) {
                 $existingFood = $order->foods->find($request->food_id);
@@ -66,22 +125,23 @@ class OrderController
                 $order->total_price += $foodPrice;
                 $order->save();
             } else {
-                $order = Order::query()->create([
+                $order = Order::create([
                     'user_id' => auth()->user()->id,
                     'restaurant_id' => $food->restaurant->id,
                     'total_price' => $foodPrice,
-                    'total_amount' => $foodPrice,
+                    // سایر فیلدها...
                 ]);
 
                 $order->foods()->attach($food, ['count' => $request->count]);
             }
 
-            return response(['Message' => 'Food added to cart successfully', 'Cart ID' => $order->id]);
+            return response(['Message' => 'Food added to cart successfully', 'Order ID' => $order->id]);
         } catch (\Exception $e) {
             \Log::error('An unexpected error occurred: ' . $e->getMessage());
             return response(['Message' => 'An unexpected error occurred. Please try again later.'], 500);
         }
     }
+
 
 
     public function update(Request $request)
@@ -122,35 +182,36 @@ class OrderController
 //        return response(['Message' => "cart number $id paid successfully"]);
 //    }
 
+//این دوتا پی کارد کار میکنن ولی میخوام سبد خرید در بیارم
 
-    public function payCard($id)
-    {
-        try {
-            $order = Order::find($id);
-
-            if (!$order) {
-                return response(['Message' => "This isn't a valid cart."], 404);
-            }
-
-            if ($order->user_id != auth()->user()->id) {
-                return response(['Message' => "You don't have permission to pay for this cart."], 403);
-            }
-
-            if ($order->customer_status == 'paid') {
-                return response(['Message' => "This cart has already been paid."], 400);
-            }
-
-            $order->customer_status = 'paid';
-            $order->seller_status = 'paid';
-            $order->save();
-
-
-            return response(['Message' => "Cart number $id paid successfully"]);
-        } catch (\Exception $e) {
-            \Log::error('An unexpected error occurred: ' . $e->getMessage());
-            return response(['Message' => 'An unexpected error occurred. Please try again later.'], 500);
-        }
-    }
+//    public function payCard($id)
+//    {
+//        try {
+//            $order = Order::find($id);
+//
+//            if (!$order) {
+//                return response(['Message' => "This isn't a valid cart."], 404);
+//            }
+//
+//            if ($order->user_id != auth()->user()->id) {
+//                return response(['Message' => "You don't have permission to pay for this cart."], 403);
+//            }
+//
+//            if ($order->customer_status == 'paid') {
+//                return response(['Message' => "This cart has already been paid."], 400);
+//            }
+//
+//            $order->customer_status = 'paid';
+//            $order->seller_status = 'paid';
+//            $order->save();
+//
+//
+//            return response(['Message' => "Cart number $id paid successfully"]);
+//        } catch (\Exception $e) {
+//            \Log::error('An unexpected error occurred: ' . $e->getMessage());
+//            return response(['Message' => 'An unexpected error occurred. Please try again later.'], 500);
+//        }
+//    }
 
 
 
